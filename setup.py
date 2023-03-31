797d4bc2e3d93385d51dea0bc30d3b33db178c4f
hparten <hallieparten@gmail.com>
2023-03-31 18:05:03 -0400

    added setup changes

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(
     version = '0.1.0',
     author = 'Hallie Parten',
     author_email = 'zsk4gm@virginia.edu',
-    packages = ['booklover', 'test'],
+    packages = ['booklover', 'booklover.test'],
     url = 'https://github.com/hparten/hw_package',
     license = 'Creative Commons',
     description = 'An awesome package that tracks evolving changes to a reader's book list',
     long_description = open('README.txt').read()
+    install_requires=['numpy >= 1.11.1', 'pandas  >= 1.5.3']
 )
