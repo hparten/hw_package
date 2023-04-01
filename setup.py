b212243f8e93e7fec220397648684057279353c8
hparten <hallieparten@gmail.com>
2023-04-01 14:02:12 -0400

    everything run!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = '0.1.0',
     author = 'Hallie Parten',
     author_email = 'zsk4gm@virginia.edu',
-    packages = ['booklover', 'booklover.test'],
+    packages = ['loverbook', 'loverbook.test'],
     url = 'https://github.com/hparten/hw_package',
     license = 'Creative Commons',
     description = 'An awesome package that tracks evolving changes to a book list',
